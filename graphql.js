c88044e3a8fa670aecdfde3327d03c089642db27
joao tiago viegas <jtviegas.js@gmail.com>
2020-08-22 23:48:55 +0200

    cleaning up files

--- graphql.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const { typeDefs, resolvers } = require('@tgedr/graphql-store-schema');
-const { ApolloServer} = require('apollo-server-lambda');
-
-const server = new ApolloServer({ typeDefs, resolvers });
-exports.handler = server.createHandler();
